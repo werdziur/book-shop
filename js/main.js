5865bd5976d929ce5394fbcabe3bae77210ac0b0
Weronika Dziura <wer.dziura@gmail.com>
2022-11-14 20:51:07 +0100

    feat: add json

--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
-fetch('../books.json') //path to the file with json data
+fetch('books.json')
 	.then(response => {
 		return response.json()
 	})
 	.then(data => {
-		console.log(data)
+		appendData(data)
 	})
 
-// const body = document.querySelector('body')
+	.catch(err => {
+		console.log('error: ' + err)
+	})
 
-const fragmentHeader = new DocumentFragment()
+function appendData(data) {
+	let main = document.querySelector('.main-container')
+	for (let book in data) {
+		const fragmentBook = new DocumentFragment()
+		const divBook = document.createElement('div')
+		const bookAuthor = document.createElement('p')
+		bookAuthor.innerHTML = `Author: ${data[book].author}`
+		const bookTitle = document.createElement('p')
+		bookTitle.innerHTML = `Title: ${data[book].title}`
+		const bookPrice = document.createElement('p')
+		bookPrice.innerHTML = `Price: ${data[book].price}`
+		const bookDescription = document.createElement('p')
+		bookDescription.innerHTML = `Description: ${data[book].description}`
+		divBook.appendChild(bookAuthor)
+		divBook.appendChild(bookTitle)
+		divBook.appendChild(bookPrice)
+		divBook.appendChild(bookDescription)
+
+		fragmentBook.appendChild(divBook)
+		main.appendChild(fragmentBook)
+		divBook.classList.add('main-container__book')
+	}
+}
+
+appendData()
+
+const fragment = new DocumentFragment()
 
 const addNav = document.createElement('nav')
 addNav.classList.add('navigation')
@@ -24,10 +52,13 @@ const headerText = document.createElement('p')
 headerText.innerText = 'A magical place for all bookworms'
 headerText.classList.add('header__text')
 
+const main = document.createElement('main')
+main.classList.add('main-container')
 
 addHeader.appendChild(headerH1)
 headerH1.appendChild(headerText)
-fragmentHeader.appendChild(addNav)
-fragmentHeader.appendChild(addHeader)
+fragment.appendChild(addNav)
+fragment.appendChild(addHeader)
+fragment.appendChild(main)
 
-document.body.prepend(fragmentHeader)
+document.body.prepend(fragment)
