bd10245a3b9080173473d20e321b9f8cf6c0a3a0
Weronika Dziua <wer.dziura@gmail.com>
2022-11-20 23:39:10 +0100

    feat: add blur event to the form

--- js/form.js
+++ js/form.js
@@ -11,7 +11,8 @@ const postcard = document.querySelector('#postcard')
 const discount = document.querySelector('#discount')
 const pen = document.querySelector('#pen')
 const submitBtn = document.querySelector('#submit-button')
-const mainContainer = document.querySelector('.main-container')
+const confirmation = document.querySelector('.order-confirmation')
+const shippingDetails = document.querySelector('#contact-details')
 
 //error spans
 const nameError = document.querySelector('#name-error')
@@ -22,104 +23,156 @@ const houseError = document.querySelector('#house-error')
 const flatError = document.querySelector('#flat-error')
 const paymentError = document.querySelector('#payment-error')
 const giftError = document.querySelector('#gift-error')
-const subbmitError = document.querySelector('#submit-error')
+const submitError = document.querySelector('#submit-error')
 
+function buttonDisabled() {
+        if (
+            !nameValidation() ||
+            !surnameValidation() ||
+            !nameValidation() ||
+            !streetValidation() ||
+            !houseNumberValidation() ||
+            !flatNumberValidation()
+        ) {
+            submitBtn.classList.add('btn-disabled')
+            submitBtn.classList.remove('btn-purchase')
+        } else {
+            submitBtn.classList.remove('btn-disabled')
+            submitBtn.classList.add('btn-purchase')
+        }
+    }
+    
+
+firstName.addEventListener('focusout', buttonDisabled)
+firstName.addEventListener('blur',() => nameValidation())
 function nameValidation() {
-	firstName.addEventListener('mouseleave', () => {
-		let name = firstName.value
-		if (name.length === 0) {
-			firstName.style.border = '2px solid #a4161a'
-			nameError.innerHTML = 'Name is required.'
-			return false
-		}
-		if (!name.match(/^[a-zA-Z]{4,30}$/)) {
-			nameError.innerHTML = 'Write full name'
-			firstName.style.border = '2px solid #a4161a'
-			return false
-		} else {
-			firstName.style.border = '1px solid green'
-			nameError.innerHTML = ''
-		}
-	})
+	let name = firstName.value
+	if (name.length === 0) {
+		firstName.style.border = '2px solid #a4161a'
+		nameError.innerHTML = 'Name is required.'
+		return false
+	}
+	if (!name.match(/^[a-zA-Z]{4,30}$/)) {
+		nameError.innerHTML = 'Write full name'
+		firstName.style.border = '2px solid #a4161a'
+		return false
+	} else {
+		firstName.style.border = '1px solid green'
+		nameError.innerHTML = ''
+		return true
+	}
 }
-nameValidation()
 
+surname.addEventListener('focusout', buttonDisabled)
+surname.addEventListener('blur', () => surnameValidation())
 function surnameValidation() {
-	surname.addEventListener('mouseleave', () => {
-		let surnameValid = surname.value
-		if (surnameValid.length === 0) {
-			surname.style.border = '2px solid #a4161a'
-			surnameError.innerHTML = 'Surname is required.'
-			return false
-		}
-		if (!surnameValid.match(/^[a-zA-Z]{5,30}$/)) {
-			surnameError.innerHTML = 'Write valid surname'
-			surname.style.border = '2px solid #a4161a'
-			return false
-		} else {
-			surname.style.border = '1px solid green'
-			surnameError.innerHTML = ''
-		}
-	})
+	let surnameValid = surname.value
+	if (surnameValid.length === 0) {
+		surname.style.border = '2px solid #a4161a'
+		surnameError.innerHTML = 'Surname is required.'
+		return false
+	}
+	if (!surnameValid.match(/^[a-zA-Z]{5,30}$/)) {
+		surnameError.innerHTML = 'Write valid surname'
+		surname.style.border = '2px solid #a4161a'
+		return false
+	} else {
+		surname.style.border = '1px solid green'
+		surnameError.innerHTML = ''
+		return true
+	}
 }
-surnameValidation()
 
+street.addEventListener('focusout', buttonDisabled)
+street.addEventListener('blur', () => streetValidation())
 function streetValidation() {
-	street.addEventListener('mouseleave', () => {
-		let streetValid = street.value
-		if (streetValid.length === 0) {
-			street.style.border = '2px solid #a4161a'
-			streetError.innerHTML = 'Street is required.'
-			return false
-		}
-		if (!streetValid.match(/^[0-9,-,#]$/)) {
-			streetError.innerHTML = 'Write full street'
-			street.style.border = '2px solid #a4161a'
-			return false
-		} else {
-			street.style.border = '1px solid green'
-			streetError.innerHTML = ''
-		}
-	})
+	let streetValid = street.value
+	if (streetValid.length === 0) {
+		street.style.border = '2px solid #a4161a'
+		streetError.innerHTML = 'Street is required.'
+		return false
+	}
+	if (!streetValid.match(/^[0-9a-zA-Z ]+$/)) {
+		streetError.innerHTML = 'Write full street'
+		street.style.border = '2px solid #a4161a'
+		return false
+	} else {
+		street.style.border = '1px solid green'
+		streetError.innerHTML = ''
+		return true
+	}
 }
-streetValidation()
 
+houseNumber.addEventListener('focusout', buttonDisabled)
+houseNumber.addEventListener('blur', () => houseNumberValidation())
 function houseNumberValidation() {
-	houseNumber.addEventListener('mouseleave', () => {
-		let houseNumberValid = houseNumber.value
-		if (houseNumberValid.length === 0) {
-			houseNumber.style.border = '2px solid #a4161a'
-			houseError.innerHTML = 'House number is required.'
-			return false
-		}
-		if (!houseNumberValid.match(/^[0-9].+$/)) {
-			houseError.innerHTML = 'Write valid house number'
-			houseNumber.style.border = '2px solid #a4161a'
-			return false
-		} else {
-			houseNumber.style.border = '1px solid green'
-			houseError.innerHTML = ''
-		}
-	})
+	let houseNumberValid = houseNumber.value
+	if (houseNumberValid.length === 0) {
+		houseNumber.style.border = '2px solid #a4161a'
+		houseError.innerHTML = 'House number is required.'
+		return false
+	}
+	if (!houseNumberValid.match(/^[0-9].+$/)) {
+		houseError.innerHTML = 'Write valid house number'
+		houseNumber.style.border = '2px solid #a4161a'
+		return false
+	} else {
+		houseNumber.style.border = '1px solid green'
+		houseError.innerHTML = ''
+		return true
+	}
 }
-houseNumberValidation()
 
+
+flatNumber.addEventListener('focusout', buttonDisabled)
+flatNumber.addEventListener('blur', () => flatNumberValidation())
 function flatNumberValidation() {
-	flatNumber.addEventListener('mouseleave', () => {
-		let flatNumberValid = flatNumber.value
-		if (flatNumberValid.length === 0) {
-			flatNumber.style.border = '2px solid #a4161a'
-			flatError.innerHTML = 'Flat number is required.'
-			return false
-		}
-		if (!flatNumberValid.match(/^[1-9–]+[-0-9–]+[#0-9]*$/)) {
-			flatError.innerHTML = 'Write valid flat number'
-			flatNumber.style.border = '2px solid #a4161a'
+	let flatNumberValid = flatNumber.value
+	if (flatNumberValid.length === 0) {
+		flatNumber.style.border = '2px solid #a4161a'
+		flatError.innerHTML = 'Flat number is required.'
+		return false
+	}
+	if (!flatNumberValid.match(/^[1-9–]+[-0-9–]+[#0-9]*$/)) {
+		flatError.innerHTML = 'Write valid flat number'
+		flatNumber.style.border = '2px solid #a4161a'
+		return false
+	} else {
+		flatNumber.style.border = '1px solid green'
+		flatError.innerHTML = ''
+		return true
+	}
+}
+
+function validateForm() {
+	submitBtn.addEventListener('click', e => {
+		if (
+			!nameValidation() ||
+			!surnameValidation() ||
+			!nameValidation() ||
+			!streetValidation() ||
+			!houseNumberValidation() ||
+			!flatNumberValidation()
+		) {
+			submitError.style.display = 'block'
+			submitError.innerHTML = 'Please fill all required fields!'
+        
+
+			setTimeout(function () {
+				submitError.style.display = 'none'
+			}, 3000)
 			return false
 		} else {
-			flatNumber.style.border = '1px solid green'
-			flatError.innerHTML = ''
+			console.log('submitted')
+            e.preventDefault()
 		}
 	})
 }
-flatNumberValidation()
+
+validateForm()
+
+// submitBtn.addEventListener('submit', e => {
+// 	shippingDetails.innerText = `${firstName.value} ${surname.value} ${street.value} ${houseNumber.value} ${flatNumber.value}`
+// })
+
+
